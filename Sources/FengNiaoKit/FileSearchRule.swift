c0080be31787b6babdb868bd05433568a6b989cb
jhonweak-aiper <jhon.wang@aiper.com>
2026-05-08 15:36:40 +0800

    Update FileSearchRule.swift
    change regex

--- Sources/FengNiaoKit/FileSearchRule.swift
+++ Sources/FengNiaoKit/FileSearchRule.swift
@@ -73,7 +73,7 @@ struct ObjCImageSearchRule: RegPatternSearchRule {
 struct SwiftImageSearchRule: RegPatternSearchRule {
     let extensions: [String]
     let patterns = [
-        #"(?:UIImage\(named:\s*|UIImage\.init\(named:\s*)"([^"]+)"\s*\)"#
+        #UIImage\(named:\s*"([^"]+)"#
     ]
 }
 
